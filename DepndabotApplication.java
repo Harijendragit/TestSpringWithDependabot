47ed809f79f24a62ac8f08a8cd75afd1c89943d7
Harijendragit <harijendrakumaryadav@gmail.com>
2026-02-08 12:38:12 +0530

    test
    test

---
+++ DepndabotApplication.java
@@ -0,0 +1,13 @@
+package com.hari.depndabot.depndabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepndabotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepndabotApplication.class, args);
+	}
+
+}
